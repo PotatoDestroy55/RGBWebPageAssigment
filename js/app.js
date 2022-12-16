1109b2241d56c22a64c0c136a0a32c547ddae2a2
Kevin Nadal <april_nadal@hotmail.com>
2022-12-16 13:06:55 -0500

    :bug: Adding buggy code that I need to fix

--- js/app.js
+++ js/app.js
@@ -3,12 +3,21 @@
 const imagen = document.getElementById('testImg');
 const output = document.getElementById('salida');
 const metaData = document.getElementById('metaData');
+var c = document.querySelector('canvas');
+var ctx = c.getContext('2d');
 const red = [];
 const green = [];
 const blue = [];
 const alpha = [];
 const idLinksImages = ["gJ8gwC8Q", "xdkxQJC0", "3xxfLbQ0", "zXjPFY61", "fRq21jM2", "0NLtykM7", "6qKYCMh3"];
 var contador = 0;
+
+//Variables para tomar el tamaño cortado
+var pointW = 0;
+var pointH = 0;
+var pointX = 0;
+var pointY = 0;
+
 
 var imageList = Array();
 
@@ -41,7 +50,7 @@ function switchImage() {
 
 
 
-imagen.addEventListener('click', function (e) {
+/*imagen.addEventListener('click', function (e) {
 	const red = [];
 	const green = [];
 	const blue = [];
@@ -107,6 +116,85 @@ imagen.addEventListener('click', function (e) {
 	Plotly.newPlot('barraVerde', data2, layout);
 	Plotly.newPlot('barraAzul', data3, layout);
 
-});
+});*/
 
+$(document).ready(function()
+{
+    $('#testImg').Jcrop(
+    {
+    	onSelect: function(c)
+    	{
+        	console.log(c);
+        	pointX = c.x;
+        	pointY = c.y;
+      		pointW = c.w;
+     		pointH = c.h;
+     		const red = [];
+			const green = [];
+			const blue = [];
+			const alpha = [];
 
+			//ctx.drawImage(imagen,0,0);
+			const pixel = ctx.getImageData(pointX, pointY, pointW, pointH).data;
+
+	/*if (!this.canvas) {
+		this.canvas = document.createElement('canvas');
+		this.canvas.width = this.width;
+		this.canvas.height = this.height;
+		ctx = this.canvas.getContext('2d');
+		ctx.drawImage(this, 0, 0, this.width, this.height);
+	} else {
+		ctx = this.canvas.getContext('2d');
+	}
+	const pixel = ctx.getImageData(pointX, pointY, pointW, pointH).data;*/
+
+	for (let i = 0; i < pixel.length; i++) {
+		red[contador] = pixel[i];
+		i += 1;
+		green[contador] = pixel[i];
+		i += 1;
+		blue[contador] = pixel[i];
+		i += 1;
+		alpha[contador] = pixel[i];
+		contador += 1;
+	}
+	console.log("RED:" + red);
+	var traceRed = {
+		x: red,
+		type: 'histogram',
+		name: "Rojo",
+		marker: {
+			color: 'red',
+		},
+	};
+	var traceGreen = {
+		x: green,
+		type: 'histogram',
+		name: "Verde",
+		marker: {
+			color: 'green',
+		},
+	};
+	var traceBlue = {
+		x: blue,
+		type: 'histogram',
+		name: "Azul",
+		marker: {
+			color: 'blue',
+		},
+	};
+
+	var layout = {
+		autosize: false,
+		width: 500,
+		height: 300,
+	};
+	var data1 = [traceRed];
+	var data2 = [traceGreen];
+	var data3 = [traceBlue];
+	Plotly.newPlot('barraRoja', data1, layout);
+	Plotly.newPlot('barraVerde', data2, layout);
+	Plotly.newPlot('barraAzul', data3, layout);
+        }
+    })
+})
